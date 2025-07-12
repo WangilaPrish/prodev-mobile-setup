acc817eacdf24fecb4a7f319cb839c60b54a464e
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-12 15:20:11 +0300

    Refactor index component to use React Native elements and improve styling

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
 
-const index = () => {
+export default function Index() {
     return (
-        <div>index</div>
-    )
+        <View style={styles.container}>
+            <Text style={styles.largeText}>Entry Screen - Awesome</Text>
+            <View>
+                <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+                <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+                <Text style={styles.smallText}>ALX is awesome</Text>
+            </View>
+        </View>
+    );
 }
 
-export default index
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#90caf9",
+        padding: 20,
+        justifyContent: 'center',
+    },
+    largeText: {
+        fontSize: 30,
+        color: "#f44336",
+        marginBottom: 5,
+        fontWeight: "700",
+        fontVariant: ["small-caps"],
+    },
+    mediumText: {
+        fontSize: 20,
+        color: "#9c27b0",
+        marginBottom: 10,
+        fontWeight: "500",
+        textAlign: "right",
+    },
+    smallText: {
+        fontSize: 15,
+        color: "#2196f3",
+        fontWeight: "400",
+        textAlign: "center",
+    },
+});
