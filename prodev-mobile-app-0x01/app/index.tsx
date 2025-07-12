f912c628326fbd4eda2bf6eea21ee6883f6cf152
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-12 14:59:22 +0300

    Refactor HomeScreen component layout and styles for improved readability

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,6 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Entry Screen - Awesome</Text>
+
+            <View>
+                <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+                <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+                <Text style={styles.smallText}>ALX is awesome</Text>
+            </View>
+        </View>
+    );
+}
+
 const styles = StyleSheet.create({
     container: {
         backgroundColor: "#90caf9",
+        padding: 20,
+        flex: 1,
+        justifyContent: "center",
     },
     largeText: {
         fontSize: 30,
