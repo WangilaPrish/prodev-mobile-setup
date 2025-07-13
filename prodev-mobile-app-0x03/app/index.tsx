8757a48d2915060e31316ffc82b1d49efccb8b14
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-13 13:37:16 +0300

    fix: update image paths to use relative references in index.tsx

--- prodev-mobile-app-0x03/app/index.tsx
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
             <SafeAreaView style={styles.container}>
                 <View style={styles.navGroup}>
                     <Ionicons name="arrow-back" size={25} />
-                    <Image source={require('@/assets/images/logo.png')} />
+                    <Image source={require("../assets/images/logo.png")} />
                 </View>
                 <Text style={styles.largeText}>Sign in to your</Text>
                 <Text style={styles.largeText}>Account</Text>
@@ -37,47 +37,23 @@ export default function Index() {
                 </TouchableOpacity>
 
                 <View style={styles.dividerGroup}>
-                    <View
-                        style={styles.divider}
-                    ></View>
-                    <Text
-                        style={styles.dividerText}
-                    >
-                        OR
-                    </Text>
-                    <View
-                        style={styles.divider}
-                    ></View>
+                    <View style={styles.divider}></View>
+                    <Text style={styles.dividerText}>OR</Text>
+                    <View style={styles.divider}></View>
                 </View>
 
                 <View style={styles.socialMediaButtonGroup}>
                     <TouchableOpacity style={styles.socialMediaButton}>
-                        <View
-                            style={{
-                                flexDirection: "row",
-                                alignItems: "center",
-                                gap: 5,
-                            }}
-                        >
-                            <Image source={require("@/assets/images/google.png")} />
-                            <Text style={styles.socialMediaButtonText}>
-                                Continue with Google
-                            </Text>
+                        <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+                            <Image source={require("../assets/images/google.png")} />
+                            <Text style={styles.socialMediaButtonText}>Continue with Google</Text>
                         </View>
                     </TouchableOpacity>
 
                     <TouchableOpacity style={styles.socialMediaButton}>
-                        <View
-                            style={{
-                                flexDirection: "row",
-                                alignItems: "center",
-                                gap: 5,
-                            }}
-                        >
-                            <Image source={require("@/assets/images/facebook.png")} />
-                            <Text style={styles.socialMediaButtonText}>
-                                Continue with Facebook
-                            </Text>
+                        <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+                            <Image source={require("../assets/images/facebook.png")} />
+                            <Text style={styles.socialMediaButtonText}>Continue with Facebook</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
