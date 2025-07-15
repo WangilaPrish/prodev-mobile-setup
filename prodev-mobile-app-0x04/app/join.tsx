f5b685367653243b7e1a575ad6333ffe0c6cfef7
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-15 16:41:24 +0300

    feat: implement Join and Sign In screens with form functionality and styles

--- prodev-mobile-app-0x04/app/join.tsx
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
+import { styles } from "@/styles/signin"; // Reuse signin styles for now
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
-const join = () => {
+export default function Join() {
     return (
-        <div>join</div>
-    )
-}
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.navGroup}>
+                    <Ionicons name="arrow-back" size={25} />
+                    <Image source={require("../assets/images/Logo.png")} />
+                </View>
+                <Text style={styles.largeText}>Create your</Text>
+                <Text style={styles.largeText}>Account</Text>
+                <Text style={styles.smallText}>
+                    Enter your details below to create your account.
+                </Text>
 
-export default join
+                <View style={styles.formGroup}>
+                    <View>
+                        <Text style={styles.placeholderText}>Full Name</Text>
+                        <TextInput style={styles.inputField} />
+                    </View>
+                    <View style={{ marginTop: 20 }}>
+                        <Text style={styles.placeholderText}>Email</Text>
+                        <TextInput keyboardType="email-address" style={styles.inputField} />
+                    </View>
+                    <View style={{ marginTop: 20 }}>
+                        <Text style={styles.placeholderText}>Password</Text>
+                        <View style={styles.passwordGroup}>
+                            <TextInput style={{ flex: 1 }} secureTextEntry />
+                            <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+                        </View>
+                    </View>
+                </View>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Join now</Text>
+                </TouchableOpacity>
+
+                <View style={styles.dividerGroup}>
+                    <View style={styles.divider}></View>
+                    <Text style={styles.dividerText}>OR</Text>
+                    <View style={styles.divider}></View>
+                </View>
+
+                <View style={styles.socialMediaButtonGroup}>
+                    <TouchableOpacity style={styles.socialMediaButton}>
+                        <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+                            <Image source={require("../assets/images/google.png")} />
+                            <Text style={styles.socialMediaButtonText}>Continue with Google</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.socialMediaButton}>
+                        <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+                            <Image source={require("../assets/images/facebook.png")} />
+                            <Text style={styles.socialMediaButtonText}>Continue with Facebook</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.subTextGroup}>
+                    <Text>Already have an account?</Text>
+                    <Text style={styles.subTextJoin}>Sign in</Text>
+                </View>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
+}
